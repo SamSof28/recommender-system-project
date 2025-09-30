0e67f7671c7238ca130f93bb0c6a05c30ea7ff49
Samuel Romaña <wassamuel@gmail.com>
2025-09-30 13:15:42 -0500

    Actualiza los datos de calificaciones de 'Estudiante_C' y añade un nuevo recurso de estudio recomendado por 'Estudiante_C' en el archivo mock_data.py.

--- recommender_backend/data/mock_data.py
+++ recommender_backend/data/mock_data.py
@@ -15,13 +15,14 @@ ACADEMIC_DATA = {
 GRADES_DATA = {
     'Estudiante_A': {'carrera': 'Ingenieria de Sistemas', 'semestre': 2, 'grades': {'Algebra Lineal': 4.75, 'Calculo Diferencial': 4.4}},
     'Estudiante_B': {'carrera': 'Ingenieria de Sistemas', 'semestre': 2, 'grades': {'Algebra Lineal': 4, 'Calculo Diferencial': 4.25}},
-    'Estudiante_C': {'carrera': 'Ingenieria de Sistemas', 'semestre': 2, 'grades': {'Algoritmos y Programación Orientada a Objetos': 4.5, 'Calculo Diferencial': 4.75}},
+    'Estudiante_C': {'carrera': 'Ingenieria de Sistemas', 'semestre': 2, 'grades': {'Algebra Lineal': 4.5, 'Calculo Diferencial': 4.75}},
 }
 
 #Recursos de estudio con su tipo y enlace
 RESOURCES_DATA: dict[str, list[dict[str, str]]] = {
     "Algebra Lineal": [
         {"type": "video", "title": 'Introducción al Algebra Lineal', "link": "https://youtu.be/0Ndnzx6AyaA?si=Qkd4d8JkHb6I8_bg", 'recommended_by': 'Estudiante_A'},
+        {"type": "video", "title": 'Curso de Matrices y Determinantes', "link": "https://www.youtube.com/watch?v=RJ96S2Pt3qU&list=PLeySRPnY35dEr2XewNdOjOl7Ft0tLIlKI", 'recommended_by': 'Estudiante_C'},
         {"type": "libro", "title": 'FUNDAMENTOS DE ÁLGEBRA LINEAL', "link": "https://recommender-api-ffpd.onrender.com/api/documents/Ingeniería de Sistemas/algebra_lineal/Fundamentos_de_álgebra_lineal-Ron_Larson.pdf", 'recommended_by': 'Estudiante_A'},
         
     ]
